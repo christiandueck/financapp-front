df44751cb95e38941e12c1f4be74e0ca86424f69
Christian Dueck <christian@dueck.com.br>
2021-06-21 09:00:52 -0300

    configure Chakra-UI, colors and font

---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#202727",
+      "800": "#2E3838",
+      "700": "#434C4C",
+      "600": "#565E5E",
+      "100": "#C7C9C9",
+      "50": "#DDDFDF"
+    }
+  },
+  fonts: {
+    heading: 'Lato',
+    body: 'Lato',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.800',
+        color: 'White'
+      }
+    }
+  }
+})
