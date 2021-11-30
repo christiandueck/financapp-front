32d3cb63faec9859324b14b9de05d4658507445e
Christian Dueck <christian@dueck.com.br>
2021-11-30 09:34:54 -0300

    [fix] filter active accounts

--- src/hooks/useAccount.tsx
+++ src/hooks/useAccount.tsx
@@ -61,9 +61,8 @@ export function AccountProvider(props: AccountProvider) {
 				a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
 			)
 			setAccounts(sortedAccounts)
-			setActiveAccounts(sortedAccounts.filter(item => item.active = true))
+			setActiveAccounts(sortedAccounts.filter(item => item.active === true))
 		})
-		console.log(accounts, activeAccounts)
 	}
 
 	function openAccountModal(account = null) {
